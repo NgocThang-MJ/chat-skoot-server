da59f899a01d51cb713ddaf86ca8fde2cbd0d958
NgocThang-MJ <tranngocthang.bkdn@gmail.com>
2021-07-06 12:52:51 +0700

    add dotenv

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
+require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
@@ -9,7 +10,6 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: { origin: process.env.CLIENT_URL },
 });
-
 // io.on("connection", (socket: Socket) => {
 //   console.log(socket.id);
 // });
